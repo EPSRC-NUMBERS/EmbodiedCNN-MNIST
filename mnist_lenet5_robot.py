db059b1de8b3c1dfff97ea6dda9e25c34e6e673e
EPSRC-NUMBERS <a.dinuovo@shu.ac.uk>
2020-02-01 18:55:11 +0000

    final test

--- mnist_lenet5_robot.py
+++ mnist_lenet5_robot.py
@@ -66,7 +66,7 @@ y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the label
 
 reps = 21
 ssplit = np.array([128,256,512,1024,3200,6400,60000]) # number of examples
-oweights = np.array([1,1,0.8,0.4,0.3,0.3,0.1])
+oweights = np.array([1,1,0.8,0.4,0.2,0.2,0.1])
 nsplit = ssplit.shape[0]
 score = np.zeros(shape=(nsplit,7))
 acc1 = np.zeros(shape=(reps,nsplit))
@@ -112,10 +112,10 @@ for k in range(reps):
 		o = BatchNormalization(name='block_norm1')(o)
 		o = Dropout(drop_prob_2, name="hidden_dropout1")(o)
 		o = Dense(84, kernel_initializer='he_uniform', activation='relu')(o) # Hidden ReLU layer
-		# o = BatchNormalization(name='block_norm2')(o)
-		# o = Dropout(drop_prob_2, name="hidden_dropout2")(o)
 		o = concatenate([o, o2],axis=1,name="concatenate") 
-		layerc = Dense(num_classes, activation='softmax', kernel_initializer='random_uniform', name='class_output')(o) # Output softmax layer
+		o = BatchNormalization(name='block_norm2')(o)
+		o = Dropout(drop_prob_2, name="hidden_dropout2")(o)
+		layerc = Dense(num_classes, activation='softmax', kernel_initializer='glorot_uniform', name='class_output')(o) # Output softmax layer
 
 		model = Model(inputs=[inp],outputs=[layerc,o2])
 
